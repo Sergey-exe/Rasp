5edf7dcd2e8d151c314c1c03563bf820bad9bf82
Draft <140250607+Sergey-exe@users.noreply.github.com>
2024-11-23 13:38:43 +0300

    Update Gun.cs

--- Gun.cs
+++ Gun.cs
@@ -1,65 +1,52 @@
-using System;
-using System.Collections.Generic;
-
-namespace Test_console_project
+class Weapon
 {
-    internal class Program
+    private List<Bullet> _bullets;
+
+    public void Fire()
     {
-        static void Main(string[] args)
-        {
-        }
-
-        class Weapon
-        {
-            private List<Bullet> _bullets;
-
-            public void Fire()
-            {
-                if (_bullets.Count == 0)
-                    return;
+        if (_bullets.Count == 0)
+            return;
 
 
-                _bullets[0].Move();
-                _bullets.RemoveAt(0);
-            }
-        }
+        _bullets[0].Move();
+        _bullets.RemoveAt(0);
+    }
+}
 
-        class Bullet
-        {
-            private int _damage;
+class Bullet
+{
+    private int _damage;
 
-            public void Move() { }
+    public void Move() { }
 
-            public void Collision(Player other)
-            {
-                other.TakeDamage(_damage);
-            }
-        }
+    public void Collision(Player other)
+    {
+        other.TakeDamage(_damage);
+    }
+}
 
-        class Player
-        {
-            private int _health;
+class Player
+{
+    private int _health;
 
-            public void TakeDamage(int damage) 
-            { 
-                if (damage < 0)
-                    damage = 0;
+    public void TakeDamage(int damage) 
+    { 
+        if (damage < 0)
+            damage = 0;
 
-                _health -= damage;
+        _health -= damage;
 
-                if (_health < 0)
-                    _health = 0;
-            }
-        }
+        if (_health < 0)
+            _health = 0;
+    }
+}
 
-        class Bot
-        {
-            private Weapon _weapon;
+class Bot
+{
+    private Weapon _weapon;
 
-            public void OnSeePlayer()
-            {
-                _weapon.Fire();
-            }
-        }
+    public void OnSeePlayer()
+    {
+        _weapon.Fire();
     }
 }
