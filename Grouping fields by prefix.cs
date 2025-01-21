a66b640a3e3821eef0a4af5fbbbc89a6e5e5b27b
Draft <140250607+Sergey-exe@users.noreply.github.com>
2025-01-21 14:14:49 +0300

    Update and rename Program.cs to  Grouping fields by prefix.cs

---
+++ Grouping fields by prefix.cs
@@ -0,0 +1,37 @@
+class Player
+{
+    public string Name { get; private set; }
+    
+    public int Age { get; private set; }
+}
+
+public class Mover
+{
+    public float MovementDirectionX { get; private set; }
+
+    public float MovementDirectionY { get; private set; }
+
+    public float MovementSpeed { get; private set; }
+
+    public void Move()
+    {
+        //Do move
+    }
+}
+
+public class Shooter
+{
+    public float WeaponCooldown { get; private set; }
+
+    public int WeaponDamage { get; private set; }
+
+    public void Attack()
+    {
+        //attack
+    }
+
+    public bool IsReloading()
+    {
+        throw new NotImplementedException();
+    }
+}
