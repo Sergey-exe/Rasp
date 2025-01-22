0eaf9d0b6743802b703b711ef683b67474045977
Draft <140250607+Sergey-exe@users.noreply.github.com>
2025-01-22 10:46:16 +0300

    Update In a function you can use functions of its level and one lower.cs

--- In a function you can use functions of its level and one lower.cs
+++ In a function you can use functions of its level and one lower.cs
@@ -1,90 +1,91 @@
-private const int MinCountSymbols = 10;
+    private const int MinCountSymbols = 10;
+    private const int CorrectErrorCode = 1;
 
-private void OnButtonClick()
-{
-    X(sender);
-}
-
-private void X(object sender)
-{
-    string rawData = passportTextbox.Text.Trim().Replace(" ", string.Empty);
-    
-    if (IsLineFull() == false)
-        return;
-
-    if (IsCorrectFormat(rawData) == false)
-        return;
-
-    TryGrantAccess(rawData);
-}
-
-private bool IsLineFull()
-{
-    if (passportTextbox.Text.Trim() != string.Empty)
+    private void OnButtonClick()
     {
-        int num1 = (int)MessageBox.Show("Введите серию и номер паспорта");
-
-        return false;
+        TryGrantAccess(sender);
     }
 
-    return true;
-}
-
-private bool IsCorrectFormat(string rawData)
-{
-    if (rawData.Length < MinCountSymbols)
+    private void TryGrantAccess(object sender)
     {
-        textResult.Text = "Неверный формат серии или номера паспорта";
-
-        return false;
-    }
-
-    return true;
-}
-
-private void TryGrantAccess(string rawData)
-{
-    string commandText = string.Format("select * from passports where num='{0}' limit 1;", (object)Form1.ComputeSha256Hash(rawData));
-    string connectionString = string.Format("Data Source=" + Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\db.sqlite");
-
-    try
-    {
-        GrantAccess(commandText, connectionString);
-    }
-    catch (SQLiteException ex)
-    {
-        if (ex.ErrorCode != 1)
+        string rawData = passportTextbox.Text.Trim().Replace(" ", string.Empty);
+        
+        if (IsLineFull() == false)
             return;
 
-        int num2 = (int)MessageBox.Show("Файл db.sqlite не найден. Положите файл в папку вместе с exe.");
+        if (IsCorrectFormat(rawData) == false)
+            return;
+
+        TryGrantAccess(rawData);
     }
-}
 
-private void GrantAccess(string commandText, string connectionString)
-{
-    SQLiteConnection connection = new SQLiteConnection(connectionString);
-    connection.Open();
-    SQLiteDataAdapter sqLiteDataAdapter = new SQLiteDataAdapter(new SQLiteCommand(commandText, connection));
-
-    DataTable dataTable1 = new DataTable();
-    DataTable dataTable2 = dataTable1;
-
-    sqLiteDataAdapter.Fill(dataTable2);
-
-    if (dataTable1.Rows.Count > 0)
+    private bool IsLineFull()
     {
-        if (Convert.ToBoolean(dataTable1.Rows[0].ItemArray[1]))
-            textResult.Text = $"По паспорту «{passportTextbox.Text}» " + 
-                $"доступ к бюллетеню на дистанционном электронном голосовании ПРЕДОСТАВЛЕН";
+        if (passportTextbox.Text.Trim() != string.Empty)
+        {
+            int num1 = (int)MessageBox.Show("Введите серию и номер паспорта");
+
+            return false;
+        }
+
+        return true;
+    }
+
+    private bool IsCorrectFormat(string rawData)
+    {
+        if (rawData.Length < MinCountSymbols)
+        {
+            textResult.Text = "Неверный формат серии или номера паспорта";
+
+            return false;
+        }
+
+        return true;
+    }
+
+    private void TryGrantAccess(string rawData)
+    {
+        string commandText = string.Format("select * from passports where num='{0}' limit 1;", (object)Form1.ComputeSha256Hash(rawData));
+        string connectionString = string.Format("Data Source=" + Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\db.sqlite");
+
+        try
+        {
+            GrantAccess(commandText, connectionString);
+        }
+        catch (SQLiteException ex)
+        {
+            if (ex.ErrorCode != CorrectErrorCode)
+                return;
+
+            int num2 = (int)MessageBox.Show("Файл db.sqlite не найден. Положите файл в папку вместе с exe.");
+        }
+    }
+
+    private void GrantAccess(string commandText, string connectionString)
+    {
+        SQLiteConnection connection = new SQLiteConnection(connectionString);
+        connection.Open();
+        SQLiteDataAdapter sqLiteDataAdapter = new SQLiteDataAdapter(new SQLiteCommand(commandText, connection));
+
+        DataTable dataTable1 = new DataTable();
+        DataTable dataTable2 = dataTable1;
+
+        sqLiteDataAdapter.Fill(dataTable2);
+
+        if (dataTable1.Rows.Count > 0)
+        {
+            if (Convert.ToBoolean(dataTable1.Rows[0].ItemArray[1]))
+                textResult.Text = $"По паспорту «{passportTextbox.Text}» " + 
+                    $"доступ к бюллетеню на дистанционном электронном голосовании ПРЕДОСТАВЛЕН";
+            else
+                textResult.Text = $"По паспорту «{passportTextbox.Text}» " +
+                    $"доступ к бюллетеню на дистанционном электронном голосовании НЕ ПРЕДОСТАВЛЯЛСЯ";
+        }
         else
-            textResult.Text = $"По паспорту «{passportTextbox.Text}» " +
-                $"доступ к бюллетеню на дистанционном электронном голосовании НЕ ПРЕДОСТАВЛЯЛСЯ";
-    }
-    else
-    {
-        textResult.Text = $"Паспорт «{passportTextbox.Text}» " +
-            $"в списке участников дистанционного голосования НЕ НАЙДЕН";
-    }
+        {
+            textResult.Text = $"Паспорт «{passportTextbox.Text}» " +
+                $"в списке участников дистанционного голосования НЕ НАЙДЕН";
+        }
 
-    connection.Close();
-}
+        connection.Close();
+    }
