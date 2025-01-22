186f103dc2b8b5918a8f403bb1b6e8c5aa7a6190
Draft <140250607+Sergey-exe@users.noreply.github.com>
2025-01-22 13:46:18 +0300

    Update When naming it makes sense to use simplified English.cs

--- When naming it makes sense to use simplified English.cs
+++ When naming it makes sense to use simplified English.cs
@@ -1,5 +1,11 @@
-public static int Find(int[] array, int element)
+public static int FindElement(int[] array, int element)
 {
+    if (array == null)
+        throw new ArgumentNullException();
+
+    if (element < 0)
+        throw new ArgumentOutOfRangeException();
+
     for (int i = 0; i < array.Length; i++)
         if (array[i] == element)
             return i;
