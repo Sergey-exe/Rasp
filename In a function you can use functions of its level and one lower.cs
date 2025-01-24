e9c8526198c94f68ba228d86c1b5c4a45dbab85c
Draft <140250607+Sergey-exe@users.noreply.github.com>
2025-01-24 17:15:02 +0300

    Update In a function you can use functions of its level and one lower.cs

--- In a function you can use functions of its level and one lower.cs
+++ In a function you can use functions of its level and one lower.cs
@@ -6,7 +6,56 @@ using System.Security.Cryptography;
 using System.Text;
 using Microsoft.Data.Sqlite;
 
-class Program
+//class Work
+//{
+//    private void checkButton_Click(object sender, EventArgs e)
+//    {
+//        if (this.passportTextbox.Text.Trim() == "")
+//        {
+//            int num1 = (int)MessageBox.Show("Введите серию и номер паспорта");
+//        }
+//        else
+//        {
+//            string rawData = this.passportTextbox.Text.Trim().Replace(" ", string.Empty);
+//            if (rawData.Length < 10)
+//            {
+//                this.textResult.Text = "Неверный формат серии или номера паспорта";
+//            }
+//            else
+//            {
+//                string commandText = string.Format("select * from passports where num='{0}' limit 1;", (object)Form1.ComputeSha256Hash(rawData));
+//                string connectionString = string.Format("Data Source=" + Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\db.sqlite");
+//                try
+//                {
+//                    SqliteConnection connection = new SqliteConnection(connectionString);
+//                    connection.Open();
+//                    SQLiteDataAdapter sqLiteDataAdapter = new SQLiteDataAdapter(new SQLiteCommand(commandText, connection));
+//                    DataTable dataTable1 = new DataTable();
+//                    DataTable dataTable2 = dataTable1;
+//                    sqLiteDataAdapter.Fill(dataTable2);
+//                    if (dataTable1.Rows.Count > 0)
+//                    {
+//                        if (Convert.ToBoolean(dataTable1.Rows[0].ItemArray[1]))
+//                            this.textResult.Text = "По паспорту «" + this.passportTextbox.Text + "» доступ к бюллетеню на дистанционном электронном голосовании ПРЕДОСТАВЛЕН";
+//                        else
+//                            this.textResult.Text = "По паспорту «" + this.passportTextbox.Text + "» доступ к бюллетеню на дистанционном электронном голосовании НЕ ПРЕДОСТАВЛЯЛСЯ";
+//                    }
+//                    else
+//                        this.textResult.Text = "Паспорт «" + this.passportTextbox.Text + "» в списке участников дистанционного голосования НЕ НАЙДЕН";
+//                    connection.Close();
+//                }
+//                catch (SQLiteException ex)
+//                {
+//                    if (ex.ErrorCode != 1)
+//                        return;
+//                    int num2 = (int)MessageBox.Show("Файл db.sqlite не найден. Положите файл в папку вместе с exe.");
+//                }
+//            }
+//        }
+//    }
+//}
+
+public partial class Program
 {
     public static void Main()
     {
@@ -87,7 +136,7 @@ public class Presenter
 
         Passport passport = new Passport(series);
 
-        _database.X(rawData, passport);
+        _database.GrantAccess(rawData, passport);
     }
 
     private bool ThereSeries(string series)
@@ -132,7 +181,7 @@ public class DataBase
         _connector = new SqliteConnector();
     }
 
-    public void X(string rawData, Passport passport)
+    public void GrantAccess(string rawData, Passport passport)
     {
         int row = 0;
         int index = 1;
