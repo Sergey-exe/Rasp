a170cdcc25048d3ae8821291760f2b12f98c1b6a
Draft <140250607+Sergey-exe@users.noreply.github.com>
2025-01-30 13:02:22 +0300

    Update Replacing conditional logic with polymorphism.cs

--- Replacing conditional logic with polymorphism.cs
+++ Replacing conditional logic with polymorphism.cs
@@ -7,10 +7,29 @@ namespace IMJunior
     {
         static void Main(string[] args)
         {
-            PayService payService = new PayService();
-            payService.Pay(Console.ReadLine());
+            OrderForm orderForm = new OrderForm();
+            PayService payService = new PayService(orderForm);
+            payService.Start();
 
             Console.ReadKey();
+        }
+    }
+
+    public class OrderForm
+    {
+        public string ShowForm(string namesSystems)
+        {
+            ShowInfo($"Мы принимаем: {namesSystems}");
+
+            // симуляция веб интерфейса
+            ShowInfo("Какое системой вы хотите совершить оплату?");
+
+            return Console.ReadLine();
+        }
+
+        public void ShowInfo(string text)
+        {
+            Console.Write(text);
         }
     }
 
@@ -20,10 +39,14 @@ namespace IMJunior
         private const string CommandWebMoney = "WebMoney";
         private const string CommandCard = "Card";
 
+        private OrderForm _orderForm;
+
         private IReadOnlyDictionary<string, IPaymentSystemFactory> _paymentFactories;
 
-        public PayService()
+        public PayService(OrderForm orderForm)
         {
+            _orderForm = orderForm ?? throw new ArgumentNullException();
+
             _paymentFactories = new Dictionary<string, IPaymentSystemFactory>()
             {
                 { CommandQIWI, new QIWIFactory() },
@@ -32,13 +55,32 @@ namespace IMJunior
             };
         }
 
-        public void Pay(string command)
+        public void Start()
+        {
+            try
+            {
+                Pay(_orderForm.ShowForm($"{CommandQIWI} {CommandWebMoney} {CommandCard}"));
+            }
+            catch (Exception exception)
+            {
+                _orderForm.ShowInfo(exception.Message);
+            }
+        }
+
+        private void Pay(string command)
         {
             if (_paymentFactories.ContainsKey(command) == false)
                 throw new ArgumentException("Попробуйте выбрать другую платёжную систему");
 
+            _orderForm.ShowInfo($"Связь с банком {_paymentFactories[command]}...");
             PaymentHandler paymentHandler = new PaymentHandler(_paymentFactories[command]);
-            paymentHandler.Pay();
+
+            _orderForm.ShowInfo($"Проверка платежа через {_paymentFactories[command]}...");
+
+            if (paymentHandler.Pay())
+                _orderForm.ShowInfo("Оплата прошла успешно!");
+
+            throw new InvalidOperationException("К сожалению, оплата не прошла. Попробуйте ещё раз, или напишите в техподдержку");
         }
     }
 
@@ -54,12 +96,12 @@ namespace IMJunior
             _paymentSystem = paymentFactory.Create();
         }
 
-        public void Pay()
+        public bool Pay()
         {
             if (_paymentSystem == null)
                 throw new ArgumentNullException();
 
-            _paymentSystem.Payment();
+            return _paymentSystem.Payment();
         }
     }
 
